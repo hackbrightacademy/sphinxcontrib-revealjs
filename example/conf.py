d02e6afc6033b78635fe99f67bcce2316adf4dc6
Ashley Trinh <ashley@hackbrightacademy.com>
2021-09-22 08:59:31 -0700

    Update example/

--- example/conf.py
+++ example/conf.py
@@ -1,5 +1,14 @@
-extensions = ["sphinxcontrib.revealjs"]
+import sys
+from pathlib import Path
+
+
+sys.path.append(
+    str((Path(__file__).parent / Path("../sphinxcontrib")).resolve())
+)
+
+extensions = ["revealjs"]
 html_sidebars = {"**": []}
 html_domain_indices = False
 html_use_index = False
 exclude_patterns = ["env", "_build"]
+revealjs_theme_options = {"revealjs_theme": "solarized.css"}
