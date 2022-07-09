b44da0b9873d5c7e3faec11d6ef48ae0efa0a5b1
Ashley Trinh <ashley@hackbrightacademy.com>
2022-07-08 17:26:10 -0700

    Remove unused transforms/functions

--- sphinxcontrib/revealjs/transforms.py
+++ sphinxcontrib/revealjs/transforms.py
@@ -2,22 +2,8 @@
 
 from sphinx.application import Sphinx
 from docutils import nodes
-from sphinx.transforms.post_transforms import SphinxPostTransform
 
 from . import addnodes
-
-
-def get_node_depth(node: nodes.section) -> int:
-    """Return depth of section, relative to document root."""
-
-    depth = 1
-    current = node
-    while not isinstance(current.parent, nodes.document):
-        depth += 1
-
-        current = current.parent
-
-    return depth
 
 
 def migrate_transitions_to_newslides(
@@ -34,53 +20,6 @@ def migrate_transitions_to_newslides(
 
     for node in doctree.traverse(nodes.transition):
         node.replace_self(addnodes.newslide("", localtitle=""))
-
-
-class UnwrapSectionNodes(SphinxPostTransform):
-    """We don't want to write nested sections."""
-
-    default_priority = 900
-
-    def mark_section_depths(self):
-        for node in self.document.traverse(nodes.section):
-            node["data-depth"] = get_node_depth(node)
-
-    def mark_newslide_depths(self):
-        for node in self.document.traverse(addnodes.newslide):
-            node["data-depth"] = node.parent.get("data-depth", 1)
-
-    def run(self) -> None:
-        self.mark_section_depths()
-        self.mark_newslide_depths()
-
-        children = []
-        for node in reversed(
-            list(
-                self.document.children[0].traverse(
-                    lambda n: isinstance(n, nodes.section)
-                    and n.get("data-depth") in self.config.revealjs_autobreak
-                )
-            )
-        ):
-            children.append(node.deepcopy())
-            node.parent.remove(node)
-
-        self.document.extend(reversed(children))
-
-
-class AddNewslideTitles(SphinxPostTransform):
-    """Add title nodes to newslides."""
-
-    default_priority = 901
-
-    def run(self) -> None:
-        for node in self.document.traverse(addnodes.newslide):
-            if not node["localtitle"]:
-                # Copy title from parent
-                title = node.parent[
-                    node.parent.first_child_matching_class(nodes.title)
-                ]
-                node["localtitle"] = title.astext()
 
 
 def process_newslides(app: Sphinx, doctree: nodes.document, _) -> None:
